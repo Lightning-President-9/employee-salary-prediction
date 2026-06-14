55fcab3aa395f1c31bb56061028331202cceedc6
Lightning-President-9 <122207673+Lightning-President-9@users.noreply.github.com>
2026-06-14 18:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,12 +74,5 @@ def predict():
         "prediction": salary
     })
 
-@app.route("/service-status", methods=["GET"])
-def status():
-    return jsonify({
-        "status": "ok",
-        "service": "Employee Salary Prediction Service API"
-    })
-
 if __name__ == "__main__":
     app.run()
